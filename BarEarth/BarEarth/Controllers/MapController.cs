d9539ee65cc9737afc850b0ec6f52381118e9bc5
Molyn Eriksson <molyneriksson@gmail.com>
2018-04-17 14:00:55 +0200

    Added map to project

---
+++ BarEarth/BarEarth/Controllers/MapController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarEarth.Controllers
+{
+    public class MapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Map()
+        {
+            ViewData["Info"] = "test";
+
+            return View();
+        }
+    }
+}
