4b0cd4ceadb7f1dbe226a4fceebc218f9e08c397
Oscar Wolter <Oscardawolter@gmail.com>
2018-05-24 17:05:41 +0200

    Begun work on the admin pages frontend. Some bug that fredrik will fix.

--- BarEarth/BarEarth/Data/Migrations/20180524145040_[migration].Designer.cs
+++ BarEarth/BarEarth/Data/Migrations/20180524145040_[migration].Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BarEarth.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180524145040_[migration]")]
+    partial class migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BarEarth/BarEarth/Data/Migrations/20180524145040_[migration].cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BarEarth.Data.Migrations
+{
+    public partial class migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Bars_BarId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BarId",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Bars_BarId",
+                table: "Products",
+                column: "BarId",
+                principalTable: "Bars",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Bars_BarId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BarId",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Bars_BarId",
+                table: "Products",
+                column: "BarId",
+                principalTable: "Bars",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
